ef5b0ada5e6517ebac441b2e670b718fb883199a
shibao koichiro <kshibao@aiming-inc.com>
2016-04-25 17:57:31 +0900

    change web to flask

--- srv.py
+++ srv.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-import web
-urls=('/.*','Index')
-render=web.template.render('template')
-class Index(object):
-    def GET(self,*args,**keys):
-        d={}
-        return render.index(d)
 
-app=web.application(urls,globals())
-def main():
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.debug = True
     app.run()
 
-if __name__=='__main__':main()
+    
