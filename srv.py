49c57ec80dcdfd8e7c9b6ba5a344bc917b92744d
shibacow <shibacow@gmail.com>
2016-05-08 16:04:42 +0900

    add newer js and gexf

--- srv.py
+++ srv.py
@@ -2,11 +2,21 @@
 # -*- coding:utf-8 -*-
 
 from flask import Flask,render_template
+import os
+from glob import glob
 app = Flask(__name__)
+gexfdir=os.getcwd()+"/static/gexf/*.gexf"
+gexfs=glob(gexfdir)
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+
+@app.route('/all')
+def all():
+    basedir=os.getcwd()
+    gexf=[os.path.relpath(unicode(g,'utf-8'),basedir) for g in gexfs]
+    return render_template("all.html",gexfs=gexf)
 
 if __name__ == '__main__':
     app.debug = True
