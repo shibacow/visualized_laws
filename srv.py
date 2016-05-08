c18272a59cb9b22f9bf41e16952beea6343a092d
shibacow <shibacow@gmail.com>
2016-05-09 00:37:06 +0900

    fix gexf

--- srv.py
+++ srv.py
@@ -4,9 +4,19 @@
 from flask import Flask,render_template
 import os
 from glob import glob
+import random
+
 app = Flask(__name__)
 gexfdir=os.getcwd()+"/static/gexf/*.gexf"
 gexfs=glob(gexfdir)
+
+class Gexf(object):
+    def __init__(self,g,basedir):
+        self.gpath=unicode(g,'utf-8')
+        self.gexfpath=os.path.relpath(self.gpath,basedir)
+        cat=os.path.basename(self.gpath)
+        cat=cat.split('.')[0]
+        self.cat=cat
 
 @app.route('/')
 def hello_world():
@@ -15,11 +25,11 @@ def hello_world():
 @app.route('/all')
 def all():
     basedir=os.getcwd()
-    gexf=[os.path.relpath(unicode(g,'utf-8'),basedir) for g in gexfs]
-    return render_template("all.html",gexfs=gexf)
+    gexf=[Gexf(g,basedir) for g in gexfs]
+    random.shuffle(gexf)
+    return render_template("all.html",gexfs=gexf[:4])
 
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0')
 
-    
--- static/js/load.js
+++ static/js/load.js
@@ -20,51 +20,20 @@ function genSig(gexf,divelm){
 }
 function gexfload(gexfpath,catname){
     sigma.parsers.gexf(
-	'/static/gexf/憲法.gexf',
+	gexfpath,
 	{ // Here is the ID of the DOM element that
 	    // will contain the graph:
-	    container: 'sigma-container-憲法'
+	    container: 'sigma-container-'+catname,
+	    settings: {
+		defaultEdgeType: 'curve',
+	    }
 	},
 	function(s) {
-	    // This function will be executed when the
-	    // graph is displayed, with "s" the related
-	    // sigma instance.
-	    console.log(s);
-	    //s.graph.nodes().forEach(function(node, i, a) {
-	    // On a circle:
-	    //node.x = Math.cos(Math.PI * 2 * i / a.length);
-	    //node.y = Math.sin(Math.PI * 2 * i / a.length);
-	    // Default size:
-	    //node.size = 1;
-	    //});
-	    //s.refresh();
+	    s.graph.edges().forEach(function(edge){ 
+		edge.type = 'curve';
+	    });
+	    s.refresh();	    
 	}
     );
 }
 
-/*function init() {
-    // Instanciate sigma.js and customize rendering :
-     
-    // Parse a GEXF encoded file to fill the graph
-    // (requires "sigma.parseGexf.js" to be included)
-    //sigInst.parseGexf('/static/keiji.gexf');
-    //sigInst.parseGexf('/static/arctic.gexf');
-    var sigInsts = new Array();
-    sigInsts.push(genSig('/static/憲法.gexf','sigma-container'));
-
-    sigInsts.forEach(function(v){
-	v.draw();
-    });
-}
-*/
-/*
-function init(){
-}
-*/  
-/*
-if (document.addEventListener) {
-    document.addEventListener("DOMContentLoaded", init, false);
-} else {
-    window.onload = init;
-}
-*/
